9709c6a96473a8449d431e6da4385f0f58951fb4
Trevor Sequino <trvseq9910@gmail.com>
2014-09-15 17:18:32 -0400

    i should have committed sooner...

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DiversityReports.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,67 @@ namespace DiversityReports.Controllers
         public ActionResult Default()
         {
             return View();
+        }
+        public ActionResult PopulateGrid()
+        {
+            dynamic employee = null;
+            var db = new ADP_FeedEntities();
+
+            employee = (from p in db.Employees
+                        join s in db.EEOSurveys on p.EmployeeID equals s.EmployeeID
+                        join e in db.Ethnicities on s.EthnicityID equals e.EthnicityID
+                        select new
+                        {
+                            p.EmployeeID,
+                            p.FirstName,
+                            p.LastName,
+                            //Ethnicity = e.Ethnicity1,
+                            Viba = e.Ethnicity1,
+                            ethnicityID = s.EthnicityID
+                        });
+
+
+
+            return new JsonResult()
+            {
+                Data = employee,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
+        public ActionResult ResaveDbData(int employeeId, int ethId)
+        {
+            var db = new ADP_FeedEntities();
+            bool _success = false;
+
+            var emp = db.EEOSurveys.Where(e => e.EmployeeID.Value == employeeId).FirstOrDefault();
+
+            emp.VisuallyIdentifyAdmin = ethId;
+
+
+            //db.SaveChanges();
+            return new JsonResult()
+            {
+                Data = _success,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+        public ActionResult GetEthnicities()
+        {
+            dynamic data = null;
+            var db = new ADP_FeedEntities();
+            data = (from e in db.Ethnicities
+                    select new
+                    {
+                        ethnicityID = e.EthnicityID,
+                        ethnicity = e.Ethnicity1
+                    });
+
+            return new JsonResult()
+            {
+                Data = data,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
         }
     }
 }
--- Scripts/custom.js
+++ Scripts/custom.js
@@ -1,49 +1,133 @@
-﻿
+﻿var pathName = location.pathname.toLowerCase();
+
+// clean the page views from the url to be able to use the base url for hyperlinking
+pathName = pathName.replace("default", "");
+pathName = pathName.replace("home", "");
+
+pathName += ((pathName.substring(pathName.length - 1) != "/") ? "/" : "");
+pathName = location.protocol + "//" + location.host + pathName.replace("//", "/");
+
 $(document).on("ready", function () {
     InitializeGrid();
-
-
-
-
+    PopulateEthDropdown();
 });
 
+function PopulateEthDropdown() {
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: (pathName + "Home/GetEthnicities"),
+        cache: false,
+        success: function (data) {
+            var options = $.map(data, function (e) {
+                return "<option value=\"" + e.ethnicityID + "\">" + e.ethnicity + "</option>";
+            }).join("");
+            $("#ethnicity").html("<option value=\"\" selected=\"\">Select...</option>" + options);
+        }
+    })
+}
 
 function InitializeGrid() {
-    var colM = [
-    { title: "Last Name", width: 150, dataIndx: "lastName" },
-    { title: "First Name", width: 150, dataIndx: "firstName" },
-    { title: "Ethnicity", width: 150, dataIndx: "ethnicity" },
-    { title: "Visually Identify by Admin", width: 300, dataIndx: "viba" }
-    ];
-    
-    var cols = ""
+    var obj = { width: 1200, height: 700 };
+    var qUrl = pathName + "Home/PopulateGrid";
 
-    var dataModel = {
+    obj.colModel = [
+        { title: "Employee ID", width: 100, dataType: "string", editable: false, hidden: true},
+        { title: "Last Name", width: 200, dataType: "string", editable: false },
+        { title: "First Name", width: 200, dataType: "string", editable: false },
+        { title: "Ethnicity ID", width: 200, dataType: "string", editable: false, hidden: true },
+        { title: "Visually Identify by Admin", width: 300, dataType: "string", editable: true }];
+
+    obj.dataModel = {
         location: "remote",
         sorting: "local",
         paging: "local",
         dataType: "JSON",
         method: "GET",
-        sortIndx: "contact",
+        url: qUrl,
+        sortIndx: 1,
         sortDir: "up",
-        url: "/Home/GetColVals", //custom field in dataModel for pqGridCrud
-        getData: function (dataJSON) {
-            //debugger;
-            var data = dataJSON.data;
-            return { data: dataJSON.data };
-        }
-    }
-    var newObj = {
-        flexHeight: true,
-        flexWidth: true,
-        dataModel: dataModel,
-        bottomVisible: true,
-        colModel: colM,
-        selectionModel: { mode: 'single' },
-        editable: true,
-        scrollModel: { horizontal: false },
-        //title: "Contact Details",
-        columnBorders: true
+        rPP: 20
     };
-    var $grid = $("#grid_crud-remote").pqGridCrud(newObj);
+
+    var $grid = $("#pqDbGrid").pqGrid(obj);
+
+    $grid.pqGrid("option", "topVisible", false);
+    $grid.pqGrid("option", "bottomVisible", true);
+    $grid.pqGrid("option", "columnBorders", true);
+    $grid.pqGrid("option", "rowBorders", true);
+    $grid.pqGrid("option", "oddRowsHighlight", true);
+    $grid.pqGrid("option", "numberCell", false);
+    $grid.pqGrid("option", "flexHeight", false);
+    $grid.pqGrid("option", "flexWidth", false);
+    $grid.pqGrid("option", "scrollModel", { horizontal: true, });
+    $grid.pqGrid("option", "resizable", true);
+    $grid.pqGrid("option", "sortable", true);
+    $grid.pqGrid("option", "roundCorners", true);
+    $grid.pqGrid("option", "editable", true);
+    $grid.pqGrid("option", "selectionModel", { type: 'row', mode: 'single' });
+    $grid.pqGrid("option", "wrap", true);
+    $grid.on("pqgridcellsave", function (event, ui) { });
+    $grid.pqGrid({
+        cellDblClick: function (event, ui) {
+            // Show editing dialog.
+            $("#dlgEditProfile").dialog({
+                resizable: true,
+                height: 700,
+                width: 700,
+                modal: true,
+                buttons: {
+                    Update: function () {
+                        var DM = $grid.pqGrid("option", "dataModel");
+                        var data = DM.data;
+                        var rowIndx = getRowIndx();
+                        var row = data[rowIndx];
+
+                        row[3] = $("#ethnicity > [selected]").val();
+                        row[4] = $("#ethnicity > [selected]").text();
+
+                        $grid.pqGrid("refreshRow", { rowIndx: rowIndx }).pqGrid('setSelection', { rowIndx: rowIndx });
+                        //saveFromDbEditDlg();
+
+                        $(this).dialog("close");
+
+                    },
+                    Cancel: function () {
+                        $(this).dialog("close");
+                    }
+                },
+                open: function (event, ui) {
+                    var $grid = $("#pqDbGrid");
+                    var rowIndx = getRowIndx();
+
+                    if (rowIndx != null) {
+                        var DM = $grid.pqGrid("option", "dataModel");
+                        var data = DM.data;
+                        var row = data[rowIndx];
+                        $("#firstLastName").text(row[2] + " " + row[1]);
+                        $("#ethnicity").val(row[3]);
+                    }
+                }
+            });
+        }
+    });
+
+
 }
+
+function saveFromDbEditDlg() {
+    var DM = $grid.pqGrid("option", "dataModel");
+    var data = DM.data;
+    var row = data[rowIndx];
+
+    var param = "Home/ResaveDbData";
+    param += "?employeeId=" + row[0]; 
+    param += "&ethId=" + row[3];
+
+    var x = $.ajax({ type: "GET", dataType: "json", url: pathName + param, cache: false });
+    x.done(function (args) {
+        alert(args);
+    })
+
+}
+
