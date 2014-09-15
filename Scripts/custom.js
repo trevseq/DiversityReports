4e9e8d6c1b6f935f4b97ff3a97a5a6abba6200b7
Trevor Sequino <trvseq9910@gmail.com>
2014-09-15 13:12:07 -0400

    framework for pqgrid

--- Scripts/custom.js
+++ Scripts/custom.js
@@ -1,1 +1,49 @@
 ﻿
+$(document).on("ready", function () {
+    InitializeGrid();
+
+
+
+
+});
+
+
+function InitializeGrid() {
+    var colM = [
+    { title: "Last Name", width: 150, dataIndx: "lastName" },
+    { title: "First Name", width: 150, dataIndx: "firstName" },
+    { title: "Ethnicity", width: 150, dataIndx: "ethnicity" },
+    { title: "Visually Identify by Admin", width: 300, dataIndx: "viba" }
+    ];
+    
+    var cols = ""
+
+    var dataModel = {
+        location: "remote",
+        sorting: "local",
+        paging: "local",
+        dataType: "JSON",
+        method: "GET",
+        sortIndx: "contact",
+        sortDir: "up",
+        url: "/Home/GetColVals", //custom field in dataModel for pqGridCrud
+        getData: function (dataJSON) {
+            //debugger;
+            var data = dataJSON.data;
+            return { data: dataJSON.data };
+        }
+    }
+    var newObj = {
+        flexHeight: true,
+        flexWidth: true,
+        dataModel: dataModel,
+        bottomVisible: true,
+        colModel: colM,
+        selectionModel: { mode: 'single' },
+        editable: true,
+        scrollModel: { horizontal: false },
+        //title: "Contact Details",
+        columnBorders: true
+    };
+    var $grid = $("#grid_crud-remote").pqGridCrud(newObj);
+}
