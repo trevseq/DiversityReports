acb503ff0ecd567181b77567f03b2d4a8c54ae57
Trevor Sequino <trvseq9910@gmail.com>
2014-09-24 10:42:30 -0400

    Comments for clarity and maintanability

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,6 +13,12 @@ namespace DiversityReports.Controllers
         {
             return View();
         }
+        /// <summary>
+        /// Searches the ADP_Feed database table Employees for records where the search string is present.
+        /// Used to populate the ParamQuery grid with employees' record information.
+        /// </summary>
+        /// <param name="str">String to search for. Can be EmployeeID, FirstName, LastName, or a combination of the three</param>
+        /// <returns>JSON object array "employee" (can return a single result or multiples)</returns>
         public ActionResult PopulateGrid(string str)
         {
             dynamic employee = null;
@@ -25,7 +31,7 @@ namespace DiversityReports.Controllers
                         join t in db.Titles on p.JobTitle equals t.TitleID
                         where str.Contains(p.EmployeeID.ToString()) || str.Contains(p.FirstName) || str.Contains(p.LastName) || str.Contains(p.PreferredFirstName) || str.Contains(p.PreferredLastName)
                         //p.EmployeeID.Equals(str) || (p.FirstName.IndexOf(str) > -1) || (p.LastName.IndexOf(str) > -1)
-                            
+                        
                         select new
                         {
                             employeeID = p.EmployeeID,
@@ -46,6 +52,12 @@ namespace DiversityReports.Controllers
             };
         }
         // Needs editing...
+        /// <summary>
+        /// Saves the edited user's information back into the ADP Feed database.
+        /// </summary>
+        /// <param name="employeeId">Employee's ID number to find the appropriate record</param>
+        /// <param name="ethId">Employee's Ethnicity ID</param>
+        /// <returns>boolean "_success" as a json string</returns>
         public ActionResult ResaveDbData(int employeeId, int ethId)
         {
             //TODO: verify that title has a link to the category- if not, create one.
@@ -63,6 +75,11 @@ namespace DiversityReports.Controllers
                 JsonRequestBehavior = JsonRequestBehavior.AllowGet
             };
         }
+        /// <summary>
+        /// Gets a list of ethnicities from the Ethnicities table 
+        /// to populate the ethnicities dropdown
+        /// </summary>
+        /// <returns>JSON object array "data"</returns>
         public ActionResult GetEthnicities()
         {
             dynamic data = null;
@@ -80,6 +97,11 @@ namespace DiversityReports.Controllers
                 JsonRequestBehavior = JsonRequestBehavior.AllowGet
             };
         }
+        /// <summary>
+        /// Gets a list of job titles from the Titles table
+        /// to populate the titles dropdown
+        /// </summary>
+        /// <returns>JSON object array "data"</returns>
         public ActionResult GetTitles()
         {
             dynamic data = null;
@@ -97,6 +119,11 @@ namespace DiversityReports.Controllers
                 JsonRequestBehavior = JsonRequestBehavior.AllowGet
             };
         }
+        /// <summary>
+        /// Gets list of EEO categories from the EEO_Category table
+        /// to populate the categories dropdown
+        /// </summary>
+        /// <returns>JSON object array "data"</returns>
         public ActionResult GetCategories()
         {
             dynamic data = null;
